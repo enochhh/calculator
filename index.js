d247c37271aaa38cd1febd8044ea88f1da1b28c7
enoch harnanto <enochharnantoo@gmail.com>
2022-02-01 20:10:47 -0800

    Added eventListeners to buttons and update display functionality

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-function add() {
+const result = document.getElementById('result');
+let digit = document.querySelectorAll('.digit');
+let op = document.querySelectorAll('.op');
+let currVal = '';
 
+digit.forEach(item => {
+    item.addEventListener('click', digitInput);
+})
+op.forEach(item => {
+    item.addEventListener('click', opInput);
+})
+
+function opInput() {
+    console.log('op was clicked');
 }
 
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
-
-function operate() {
-    
+function digitInput(e) {
+    currVal += e.target.innerText;
+    result.innerText = currVal;
 }
